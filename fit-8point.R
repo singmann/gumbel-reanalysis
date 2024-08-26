7b674141b254fb7e596a626b0feb160ba54f0eb3
Henrik Singmann <singmann@gmail.com>
2024-08-26 16:44:52 +0100

    added 8-point analysis

--- fit-8point.R
+++ fit-8point.R
@@ -5,84 +5,88 @@ options(mc.cores = parallel::detectCores())
 theme_set(theme_bw(base_size = 15) + 
             theme(legend.position="bottom"))
 #load("dat-prep.rda")
-source("gumbel6agg-stan.R")
-source("uvsdt6agg-stan.R")
+source("gumbel8agg-stan.R")
+source("uvsdt8agg-stan.R")
 
-data("roc6", package = "MPTinR")
-head(roc6)
+data("roc8", package = "MPTinR")
+head(roc8)
 
-dataset6 <- levels(roc6$exp)
+dataset8 <- levels(roc8$exp)
 
-gumbel_formula <- brmsformula(
+gumbel_formula_8 <- brmsformula(
   OLD_3new ~ 1 + (1|p|id), 
   crc ~ (1|p|id), 
-  crlm ~ (1|p|id), crll ~ (1|p|id), 
-  crhm ~ (1|p|id), crhh ~ (1|p|id),
-  family = gumbel6agg_family, cmc = FALSE
+  crlm ~ (1|p|id), crll ~ (1|p|id), crlx ~ (1|p|id), 
+  crhm ~ (1|p|id), crhh ~ (1|p|id), crhx ~ (1|p|id),
+  family = gumbel8agg_family, cmc = FALSE
 )
 
-gumbel_priors <- prior(normal(0,0.5), class = Intercept, dpar = "crc") + 
+gumbel_priors_8 <- prior(normal(0,0.5), class = Intercept, dpar = "crc") + 
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crlm") +
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crll") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crlx") +
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crhm") +
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crhh") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crhx") +
   prior(student_t(3, -1, 2), class = Intercept)
 
-uvsdt_formula <- brmsformula(
+uvsdt_formula_8 <- brmsformula(
   OLD_3new ~ 1 + (1|p|id), 
   discsignal ~ 1 + (1|p|id), 
   crc ~ (1|p|id), 
-  crlm ~ (1|p|id), crll ~ (1|p|id), 
-  crhm ~ (1|p|id), crhh ~ (1|p|id),
-  family = uvsdt6agg_family, cmc = FALSE
+  crlm ~ (1|p|id), crll ~ (1|p|id), crlx ~ (1|p|id), 
+  crhm ~ (1|p|id), crhh ~ (1|p|id), crhx ~ (1|p|id),
+  family = uvsdt8agg_family, cmc = FALSE
 )
 
-uvsdt_priors <- prior(normal(0,0.5), class = Intercept, dpar = "crc") + 
+uvsdt_priors_8 <- prior(normal(0,0.5), class = Intercept, dpar = "crc") + 
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crlm") +
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crll") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crlx") +
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crhm") +
   prior(normal(-0.5,0.5), class = Intercept, dpar = "crhh") +
+  prior(normal(-0.5,0.5), class = Intercept, dpar = "crhx") +
   prior(student_t(3, 0.5, 1), class = Intercept, dpar = "discsignal") +
   prior(student_t(3, 1, 2), class = Intercept)
 
-roc6_data <- vector("list", length(dataset6))
-roc6_oldmat <- vector("list", length(dataset6))
-roc6_newmat <- vector("list", length(dataset6))
-roc6_sv <- vector("list", length(dataset6))
+roc8_data <- vector("list", length(dataset8))
+roc8_oldmat <- vector("list", length(dataset8))
+roc8_newmat <- vector("list", length(dataset8))
+roc8_sv <- vector("list", length(dataset8))
 
-roc6_fits_gumbel <- vector("list", length(dataset6))
-roc6_fits_uvsdt <- vector("list", length(dataset6))
+roc8_fits_gumbel <- vector("list", length(dataset8))
+roc8_fits_uvsdt <- vector("list", length(dataset8))
 
-
-for (i in seq_along(dataset6)) {
+#i <- 1
+for (i in seq_along(dataset8)) {
   print(i)
-  roc6_data[[i]] <- roc6 %>% 
-    filter(exp == dataset6[i])
-  roc6_oldmat[[i]] <- roc6_data[[i]] %>% 
-                                  select(OLD_3new:OLD_3old) %>% 
+  roc8_data[[i]] <- roc8 %>% 
+    filter(exp == dataset8[i])
+  roc8_oldmat[[i]] <- roc8_data[[i]] %>% 
+                                  select(OLD_4new:OLD_4old) %>% 
                                   as.matrix()
-  roc6_newmat[[i]] <- roc6_data[[i]] %>% 
-                                  select(NEW_3new:NEW_3old) %>% 
+  roc8_newmat[[i]] <- roc8_data[[i]] %>% 
+                                  select(NEW_4new:NEW_4old) %>% 
                                   as.matrix()
-  roc6_sv[[i]] <- stanvar(roc6_oldmat[[i]], name = "oldmat", block = "data") +
-    stanvar(scode = "array[N, 6] int oldmat2;", block = "tdata") +
+  roc8_sv[[i]] <- stanvar(roc8_oldmat[[i]], name = "oldmat", block = "data") +
+    stanvar(scode = "array[N, 8] int oldmat2;", block = "tdata") +
     stanvar(scode = "oldmat2 = to_int(to_array_2d(oldmat));", block = "tdata") +
-    stanvar(roc6_newmat[[i]], name = "newmat", block = "data") +
-    stanvar(scode = "array[N, 6] int newmat2;", block = "tdata") +
+    stanvar(roc8_newmat[[i]], name = "newmat", block = "data") +
+    stanvar(scode = "array[N, 8] int newmat2;", block = "tdata") +
     stanvar(scode = "newmat2 = to_int(to_array_2d(newmat));", block = "tdata")
   
-  roc6_fits_gumbel[[i]] <- brm(
-    gumbel_formula, data = roc6_data[[i]], 
-    stanvars = sv_gumbel6agg + roc6_sv[[i]], 
-    prior = gumbel_priors,
-    init_r = 0.5
+  roc8_fits_gumbel[[i]] <- brm(
+    gumbel_formula_8, data = roc8_data[[i]], 
+    stanvars = sv_gumbel8agg + roc8_sv[[i]], 
+    prior = gumbel_priors_8,
+    init_r = 0.5, control = list(adapt_delta = 0.99)
   )
   
-  roc6_fits_uvsdt[[i]] <- brm(
-    uvsdt_formula, data = roc6_data[[i]], 
-    stanvars = sv_uvsdt6agg + roc6_sv[[i]], 
-    prior = uvsdt_priors,
-    init_r = 0.5
+  roc8_fits_uvsdt[[i]] <- brm(
+    uvsdt_formula_8, data = roc8_data[[i]], 
+    stanvars = sv_uvsdt8agg + roc8_sv[[i]], 
+    prior = uvsdt_priors_8,
+    init_r = 0.5, control = list(adapt_delta = 0.99)
   )
 }
 
@@ -91,61 +95,64 @@ for (i in seq_along(dataset6)) {
 #          prior = gumbel_priors)
 
 
-roc6_loo_gumbel <- lapply(roc6_fits_gumbel, loo)
-roc6_waic_gumbel <- lapply(roc6_fits_gumbel, waic)
+## exclude data set 1 which sows problems
 
-roc6_loo_uvsdt <- lapply(roc6_fits_uvsdt, loo)
-roc6_waic_uvsdt <- lapply(roc6_fits_uvsdt, waic)
+roc8_loo_gumbel <- lapply(roc8_fits_gumbel[-1], loo)
+roc8_waic_gumbel <- lapply(roc8_fits_gumbel[-1], waic)
 
-for (i in seq_along(roc6_loo_gumbel)) {
-  attr(roc6_loo_gumbel[[i]], "model_name") <- "gumbel"
-  attr(roc6_waic_gumbel[[i]], "model_name") <- "gumbel"
+roc8_loo_uvsdt <- lapply(roc8_fits_uvsdt[-1], loo)
+roc8_waic_uvsdt <- lapply(roc8_fits_uvsdt[-1], waic)
+
+for (i in seq_along(roc8_loo_gumbel)) {
+  attr(roc8_loo_gumbel[[i]], "model_name") <- "gumbel"
+  attr(roc8_waic_gumbel[[i]], "model_name") <- "gumbel"
   
-  attr(roc6_loo_uvsdt[[i]], "model_name") <- "uvsdt"
-  attr(roc6_waic_uvsdt[[i]], "model_name") <- "uvsdt"
+  attr(roc8_loo_uvsdt[[i]], "model_name") <- "uvsdt"
+  attr(roc8_waic_uvsdt[[i]], "model_name") <- "uvsdt"
   
 }
 
-waic_comp <- mapply(loo_compare, roc6_waic_gumbel, roc6_waic_uvsdt, SIMPLIFY = FALSE)
-loo_comp <- mapply(loo_compare, roc6_loo_gumbel, roc6_loo_uvsdt, SIMPLIFY = FALSE)
+waic_comp <- mapply(loo_compare, roc8_waic_gumbel, roc8_waic_uvsdt, SIMPLIFY = FALSE)
+loo_comp <- mapply(loo_compare, roc8_loo_gumbel, roc8_loo_uvsdt, SIMPLIFY = FALSE)
 
-str(roc6_waic_gumbel[[1]])
+str(roc8_waic_gumbel[[1]])
 
-roc6_waic_gumbel[[1]]$estimates["waic","Estimate"]
+roc8_waic_gumbel[[1]]$estimates["waic","Estimate"]
 
 #mod_comp <- 
 tibble(
-  dataset = dataset6,
-  waic_g = map_dbl(roc6_waic_gumbel, ~.$estimates["waic","Estimate"]),
-  waic_uv = map_dbl(roc6_waic_uvsdt, ~.$estimates["waic","Estimate"]),
+  dataset = dataset8[-1],
+  waic_g = map_dbl(roc8_waic_gumbel, ~.$estimates["waic","Estimate"]),
+  waic_uv = map_dbl(roc8_waic_uvsdt, ~.$estimates["waic","Estimate"]),
 ) %>% 
   mutate(min_waic = pmin(waic_g, waic_uv)) %>% 
   mutate(across(c(waic_g, waic_uv), ~ format(.-min_waic, digits = 2, format = "g"))) %>% 
   mutate(waic_diff_sig = map_lgl(waic_comp, ~ abs(.[2, "elpd_diff"]) > (2*.[2, "se_diff"])))
 
 tibble(
-  dataset = dataset6,
-  looic_g = map_dbl(roc6_loo_gumbel, ~.$estimates["looic","Estimate"]),
-  looic_uv = map_dbl(roc6_loo_uvsdt, ~.$estimates["looic","Estimate"]),
+  dataset = dataset8[-1],
+  looic_g = map_dbl(roc8_loo_gumbel, ~.$estimates["looic","Estimate"]),
+  looic_uv = map_dbl(roc8_loo_uvsdt, ~.$estimates["looic","Estimate"]),
 ) %>% 
   mutate(min_waic = pmin(looic_g, looic_uv)) %>% 
   mutate(across(c(looic_g, looic_uv), ~ format(.-min_waic, digits = 2, format = "g"))) %>% 
   mutate(waic_diff_sig = map_lgl(loo_comp, ~ abs(.[2, "elpd_diff"]) > (2*.[2, "se_diff"])))
 
 ## plots
-plot_data <- roc6 %>% 
+plot_data <- roc8 %>% 
+  filter(exp != dataset8[1]) %>% 
   group_by(exp) %>% 
-  summarise(across(c(OLD_3new:NEW_3old), sum)) %>% 
+  summarise(across(c(OLD_4new:NEW_4old), sum)) %>% 
   pivot_longer(-exp, names_to = c("status", "response"), names_sep = "_") %>% 
   group_by(exp, status) %>% 
   mutate(observed = value / sum(value)) %>% 
    mutate(
     status = factor(status, levels = c("OLD", "NEW")), 
-    response = factor(response, levels = c("3new", "2new", "1new", 
-                                           "1old", "2old", "3old")))
+    response = factor(response, levels = c("4new", "3new", "2new", "1new", 
+                                           "1old", "2old", "3old", "4old")))
   
-pred_gumbel <- lapply(roc6_fits_gumbel, posterior_epred)
-pred_uvsdt <- lapply(roc6_fits_uvsdt, posterior_epred)
+pred_gumbel <- lapply(roc8_fits_gumbel[-1], posterior_epred)
+pred_uvsdt <- lapply(roc8_fits_uvsdt[-1], posterior_epred)
 
 plot_data$gumbel <- unlist(map(pred_gumbel, ~apply(., c(3), mean)))
 plot_data$uvsd <- unlist(map(pred_uvsdt, ~apply(., c(3), mean)))
@@ -173,7 +180,7 @@ plot_data %>%
   pivot_wider(names_from = status, values_from = c(observed, gumbel, uvsd)) %>% 
   arrange(exp, desc(response)) %>% 
   mutate(across(-c(response), cumsum)) %>% 
-  filter(response != "3new") %>% 
+  filter(response != "4new") %>% 
   ggplot(aes(x =  observed_NEW, y = observed_OLD)) +
   geom_abline(slope = 1, intercept = 0) +
   geom_line(aes(group = 1)) +
@@ -191,7 +198,7 @@ plot_data2 %>%
                                                    uvsd_low, uvsd_high)) %>% 
   arrange(exp, desc(response)) %>% 
   mutate(across(-c(response), cumsum)) %>% 
-  filter(response != "3new") %>% 
+  filter(response != "4new") %>% 
   ggplot(aes(x =  observed_NEW, y = observed_OLD)) +
   geom_abline(slope = 1, intercept = 0) +
    
---
+++ gumbel8agg-stan.R
@@ -0,0 +1,134 @@
+gumbel8agg_stanvars <- "
+   real gumbelmin(real x, real mu, real disc){
+     //return 1- exp(-exp(-(-x-mu)/disc));
+     //return exp(gumbel_lccdf(-x | mu,disc));
+     return 1 - gumbel_cdf(-x|mu,disc);
+   }
+   real gumbel8agg_lpmf(int y, real mu, real crc, 
+                    real crlm, real crll, real crlx, 
+                    real crhm, real crhh, real crhx,
+                    array[] int oldvec, array[] int newvec) {
+    int nthres = 7;
+    vector[nthres+1] pold;
+    vector[nthres+1] pnew;
+    
+    real disc = 1;
+    vector[nthres] thres;
+    
+  // calculate thresholds
+  thres[1] = crc - (exp(crlm) + exp(crll) + exp(crlx));
+  thres[2] = crc - (exp(crlm) + exp(crll));
+  thres[3] = crc - (exp(crlm));
+  thres[4] = crc;
+  thres[5] = crc + (exp(crhm));
+  thres[6] = crc + (exp(crhm) + exp(crhh));
+  thres[7] = crc + (exp(crhm) + exp(crhh) + exp(crhx));
+     
+    // calculate probabilities
+    pold[1] = gumbelmin(thres[1], mu, disc);
+    for (i in 2:nthres) {
+      pold[i] = gumbelmin(thres[i], mu, disc) - gumbelmin(thres[i-1], mu, disc);
+    }
+    pold[nthres+1] = 1 - gumbelmin(thres[nthres], mu, disc);
+    
+    pnew[1] = gumbelmin(thres[1], 0, disc);
+    for (i in 2:nthres) {
+      pnew[i] = gumbelmin(thres[i], 0, disc) - gumbelmin(thres[i-1], 0, disc);
+    }
+    pnew[nthres+1] = 1 - gumbelmin(thres[nthres], 0, disc);
+    return multinomial_lpmf(oldvec | pold) + multinomial_lpmf(newvec | pnew);
+   }
+"
+
+gumbel8agg_family <- custom_family(
+  name = "gumbel8agg", 
+  dpars = c("mu", "crc", "crlm", "crll", "crlx", "crhm", "crhh", "crhx"), 
+  links = c("identity", rep("identity", 7)), lb = c(NA, rep(NA, 7)),
+  type = "int", vars = c("oldmat2[n]", "newmat2[n]")
+)
+sv_gumbel8agg <- stanvar(scode = gumbel8agg_stanvars, block = "functions")
+
+calc_posterior_predictions_gumbel8agg <- function(i, prep) {
+  gumbelmin <- function(x, mu, disc) {
+    return(1 - extraDistr::pgumbel(-x,mu,disc))
+  }
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  #discsignal <- brms::get_dpar(prep, "discsignal", i = i)
+  crc <- brms::get_dpar(prep, "crc", i = i)
+  crlm <- brms::get_dpar(prep, "crlm", i = i)
+  crll <- brms::get_dpar(prep, "crll", i = i)
+  crlx <- brms::get_dpar(prep, "crlx", i = i)
+  crhm <- brms::get_dpar(prep, "crhm", i = i)
+  crhh <- brms::get_dpar(prep, "crhh", i = i)
+  crhx <- brms::get_dpar(prep, "crhx", i = i)
+
+  oldvec <- prep$data$oldmat[i,]
+  newvec <- prep$data$newmat[i,]
+  OUTLEN <- length(mu)
+  
+  disc <- 1
+  nthres <- 7
+  thres <- matrix(NA_real_, nrow = OUTLEN, ncol = nthres)
+  pold <- matrix(NA_real_, nrow = OUTLEN, ncol = nthres+1)
+  pnew <- matrix(NA_real_, nrow = OUTLEN, ncol = nthres+1)
+  
+  thres[,1] = crc - (exp(crlm) + exp(crll) + exp(crlx));
+  thres[,2] = crc - (exp(crlm) + exp(crll));
+  thres[,3] = crc - (exp(crlm));
+  thres[,4] = crc;
+  thres[,5] = crc + (exp(crhm));
+  thres[,6] = crc + (exp(crhm) + exp(crhh));
+  thres[,7] = crc + (exp(crhm) + exp(crhh) + exp(crhx));
+  
+  # calculate probabilities
+  pold[,1] = gumbelmin(thres[,1], mu, disc)
+  #ordinal::pgumbel(thres[,1], mu, disc, max = FALSE) ## does NOT 
+  for (j in 2:nthres) {
+    pold[,j] = gumbelmin(thres[,j], mu, disc) - gumbelmin(thres[,j-1], mu, disc);
+  }
+  pold[,nthres+1] = 1 - gumbelmin(thres[,nthres], mu, disc);
+
+  pnew[,1] = gumbelmin(thres[,1], 0, disc);
+  for (j in 2:nthres) {
+    pnew[,j] = gumbelmin(thres[,j], 0, disc) - gumbelmin(thres[,j-1], 0, disc);
+  }
+  pnew[,nthres+1] = 1 - gumbelmin(thres[,nthres], 0, disc);
+  return(list(
+    pold = pold, pnew = pnew
+  ))
+}
+
+log_lik_gumbel8agg <- function(i, prep) {
+  use <- calc_posterior_predictions_gumbel8agg(i = i, prep = prep)
+  oldvec <- prep$data$oldmat[i,]
+  newvec <- prep$data$newmat[i,]
+  extraDistr::dmnom(x = oldvec, size = sum(oldvec), prob = use$pold, log = TRUE) + 
+     extraDistr::dmnom(x = newvec, size = sum(newvec), prob = use$pnew, log = TRUE)
+}
+
+posterior_epred_gumbel8agg <- function(prep) {
+  nobs <- prep$nobs
+  out <- array(NA_real_, dim = c(prep$ndraws, prep$nobs, 16), 
+               dimnames = list(seq(prep$ndraws), seq(prep$nobs), 
+                               c(colnames(prep$data$oldmat), colnames(prep$data$newmat))))
+  for (i in seq_len(nobs)) {
+    tmp <- calc_posterior_predictions_gumbel8agg(i = i, prep = prep)
+    out[,i,] <- c(tmp$pold, tmp$pnew)
+  }
+  return(out)
+}
+
+posterior_predict_gumbel8agg <- function(i, prep, ...) {
+  use <- calc_posterior_predictions_gumbel8agg(i = i, prep = prep)
+  oldvec <- prep$data$oldmat[i,]
+  newvec <- prep$data$newmat[i,]
+  
+  lout <- nrow(use$pold)
+  out <- cbind(extraDistr::rmnom(n = rep(1, lout), size = sum(oldvec), prob = use$pold), 
+               extraDistr::rmnom(n = rep(1, lout), size = sum(oldvec), prob = use$pnew))
+  colnames(out) <- c(colnames(prep$data$oldmat), colnames(prep$data$newmat))
+  #browser()
+  lapply(seq_len(nrow(out)), function(i) out[i,])
+  #apply(out, 1, function(x) list(x))
+  #out[,1]
+}
